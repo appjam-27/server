44e8f39c95d1dfe84044c472543b71809ba5f15f
suk-6 <me@suk.kr>
2024-08-18 03:56:56 +0900

    feat: Add StoriesDTO for GptService

---
+++ src/gpt/dto/stories.dto.ts
@@ -0,0 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsArray, IsString } from 'class-validator';
+
+export class StoriesDTO {
+  @ApiProperty({
+    example: '초보가 피그마 천재로 성장하는 여정',
+  })
+  @IsString()
+  title: string;
+
+  @ApiProperty({
+    example: [
+      '나는 온라인 수업을 시작했다. "피그마"라는 프로그램을 배우고 싶었다.',
+      '나는 온라인 수업을 시작했다. "피그마"라는 프로그램을 배우고 싶었다.',
+      '나는 온라인 수업을 시작했다. "피그마"라는 프로그램을 배우고 싶었다.',
+    ],
+  })
+  @IsArray()
+  content: string[];
+}
