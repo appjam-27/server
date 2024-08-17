8544fe00f9cf3780a0d7e04d7c847d373dbb3da1
suk-6 <me@suk.kr>
2024-08-18 00:21:10 +0900

    refactor: Update ChapterModel and StoryModel classes with Swagger annotations and class-validator decorators

--- src/gpt/gpt.model.ts
+++ src/gpt/gpt.model.ts
@@ -1,5 +1,37 @@
-export type ChapterModel = {
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
+
+export class ChapterModel {
+  @ApiProperty({
+    example: '레이아웃과 그리드',
+  })
+  @IsString()
   title: string;
+
+  @ApiProperty({
+    example: '효율적인 레이아웃과 그리드 시스템을 활용하는 방법을 익혀봅시다.',
+  })
+  @IsString()
   desc: string;
+
+  @ApiProperty({
+    example: '1시간',
+  })
+  @IsString()
   duration: string;
-};
+}
+
+export class StoryModel {
+  @ApiProperty({
+    example: '초보가 피그마 천재로 성장하는 여정',
+  })
+  @IsString()
+  title: string;
+
+  @ApiProperty({
+    example:
+      '나는 온라인 수업을 시작했다. "피그마"라는 프로그램을 배우고 싶었다.',
+  })
+  @IsString()
+  content: string;
+}
