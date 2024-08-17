274e153c6b385dd36955b53e41dbbd05fbb2def7
suk-6 <me@suk.kr>
2024-08-18 04:29:55 +0900

    feat: Add description to StoriesDTO

--- src/gpt/dto/stories.dto.ts
+++ src/gpt/dto/stories.dto.ts
@@ -3,12 +3,14 @@ import { IsArray, IsString } from 'class-validator';
 
 export class StoriesDTO {
   @ApiProperty({
+    description: '소설 제목',
     example: '초보가 피그마 천재로 성장하는 여정',
   })
   @IsString()
   title: string;
 
   @ApiProperty({
+    description: '소설 내용들',
     example: [
       '나는 온라인 수업을 시작했다. "피그마"라는 프로그램을 배우고 싶었다.',
       '나는 온라인 수업을 시작했다. "피그마"라는 프로그램을 배우고 싶었다.',
