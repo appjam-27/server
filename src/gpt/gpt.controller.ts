98ac63c39b6c5b6b0d74ec4c4f8126041588127b
suk-6 <me@suk.kr>
2024-08-18 00:21:04 +0900

    feat: Add API endpoints for creating goals, updating stories, and getting chapter content

--- src/gpt/gpt.controller.ts
+++ src/gpt/gpt.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiBody, ApiOkResponse } from '@nestjs/swagger';
+import { NewGoalDTO, NewGoalResponseDTO } from './dto/newGoal.dto';
+import { GptService } from './gpt.service';
+import { UpdateStoryDTO, UpdateStoryResponseDTO } from './dto/updateStory.dto';
+import { ChapterContentDTO } from './dto/chapterContent.dto';
 
 @Controller('gpt')
-export class GptController {}
+export class GptController {
+  constructor(private readonly gptService: GptService) {}
+
+  @Post('goal/new')
+  @ApiBody({ type: NewGoalDTO })
+  @ApiOkResponse({ description: 'Create a new goal', type: NewGoalResponseDTO })
+  async createGoal(@Body() dto: NewGoalDTO) {
+    return {
+      goal: dto.goal,
+      icon: await this.gptService.selectIcon(dto.goal),
+      chapters: await this.gptService.makeChapter(dto.goal),
+      story: await this.gptService.makeFirstStory(dto.goal),
+    };
+  }
+
+  @Post('story/update')
+  @ApiBody({ type: UpdateStoryDTO })
+  @ApiOkResponse({
+    description: 'Update a story',
+    type: UpdateStoryResponseDTO,
+  })
+  async updateStory(@Body() dto: UpdateStoryDTO) {
+    return {
+      content: await this.gptService.makeStory(dto.goal, dto.chapters),
+    };
+  }
+
+  @Post('chapter/content')
+  @ApiBody({ type: ChapterContentDTO })
+  @ApiOkResponse({
+    description: 'Get chapter content',
+    type: ChapterContentDTO,
+  })
+  async getChapterContent(@Body() dto: ChapterContentDTO) {
+    return {
+      content: await this.gptService.makeChapterContent(dto.goal, dto.chapter),
+    };
+  }
+}
