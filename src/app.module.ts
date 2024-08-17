b7056513dd8a9a8a009f4b503667170e40b3e370
suk-6 <me@suk.kr>
2024-08-17 22:33:07 +0900

    feat: add ConfigModule for global configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GptModule } from './gpt/gpt.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [GptModule],
+  imports: [
+    GptModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
